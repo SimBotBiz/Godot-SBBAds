b884ac27ccf8fe81ffc0be0676b3bafc2f017b35
Simone Bottino <simbotbiz@gmail.com>
2019-11-05 13:41:44 +0100

    nicer code

--- sbbads/config.py
+++ sbbads/config.py
@@ -14,18 +14,14 @@ def configure(env):
 
         # play-service-ads
         if artifacts['play-services-ads']:
-            env.android_add_dependency(
-                "implementation 'com.google.android.gms:play-services-ads:18.2.0'"
-            )
+            env.android_add_dependency("implementation 'com.google.android.gms:play-services-ads:18.2.0'")
             env.android_add_java_dir("play-services-ads/java")
             env.android_add_to_manifest("play-services-ads/AndroidManifestChunk.xml")
             env.android_add_to_permissions("play-services-ads/AndroidPermissionsChunk.xml")
 
         # consent-library
         if artifacts['consent-library']:
-            env.android_add_dependency(
-                "implementation 'com.google.android.ads.consent:consent-library:1.0.7'"
-            )
+            env.android_add_dependency("implementation 'com.google.android.ads.consent:consent-library:1.0.7'")
             env.android_add_java_dir("consent-library/java")
             env.android_add_asset_dir("consent-library/asset")
             env.android_add_to_manifest("consent-library/AndroidManifestChunk.xml")
